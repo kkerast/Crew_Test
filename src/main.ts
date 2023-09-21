e44c267e0ea7e04880e3d17b213556b31c5d2333
kkerast <dlwlsgkqkq@naver.com>
2023-09-21 18:54:05 +0900

    test17

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('CREW API Document')
-    .setDescription('Crew API 서버')
+    .setDescription('Crew API 서버 ')
     .setVersion('1.0')
     .addBearerAuth(
       {
